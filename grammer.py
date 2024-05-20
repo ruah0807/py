13226f60f59980e110c48159965ad55bccb5e713
Ruah Kim <kimhk0315@gmail.com>
2024-05-20 23:27:14 +0900

    🎨 grammer of def

--- grammer.py
+++ grammer.py
@@ -1,7 +1,9 @@
 
 
-재고량 = 20
+### if 문 ###
 
+
+재고량 = 20
 
 if 재고량 > 0 : 
     print('지금 주문가능합니다.')
@@ -12,3 +14,48 @@ if 'K6' in 중고차재고 :
     print('재고 있습니다.')
 else : 
     print('주문불가능합니다.')
+
+
+
+
+
+
+
+### For 문 ###
+
+
+for i in range(0,3) :
+    print('k5 있어요')
+    
+
+중고차들 = ['Tico', 'Lexus', 'Mustang']
+
+for i in 중고차들 :
+    print(i * 3)
+
+
+
+
+
+
+
+### 함수 문법 Def : definition의 약자 (긴코드 줄이고싶을때) ###
+
+def greeting():
+    print('안녕하세요 무섭게 성장하는 신입개발자 김루아입니다^^')
+
+greeting()
+
+
+
+def hat(empty, empty2) :
+    print(empty * empty2)
+
+hat(1,4)
+hat(3,6)
+
+
+def 함수() :
+    return 10
+
+print(함수())

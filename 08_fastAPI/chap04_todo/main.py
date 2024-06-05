756b8533084ecf4018d776a5fd4e4e8e04fd3f1a
Ruah Kim <kimhk0315@gmail.com>
2024-06-05 16:30:46 +0900

    🎨 update & delete

--- 08_fastAPI/chap04_todo/main.py
+++ 08_fastAPI/chap04_todo/main.py
@@ -52,7 +52,7 @@ async def home(request : Request, db: Session = Depends(get_db)):
 
 
 @app.post("/add")               # html 에서 form 태그로 넘겨준 값 
-async def add(request:Request, task:str=Form(...), db:Session=Depends(get_db)):
+async def add( task:str=Form(...), db:Session=Depends(get_db)):
     
     # todo 클래스의 인스턴스를 생성하고, task 값을 전달하여 할일을 생성
     todo = models.Todo(task=task)
@@ -74,3 +74,31 @@ async def add(request:Request, todo_id: int, db: Session = Depends(get_db)):
     
     # 템플릿을 랜더링 하여 사용자에게 반환
     return templetes.TemplateResponse("edit.html", {"request" : request, "todo" : todo})
+
+
+
+@app.post("/edit/{todo_id}")
+async def add(todo_id:int, task:str = Form(...), completed:bool = Form(False), db:Session=Depends(get_db)):
+    
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    
+    #todo task 속성을 폼에서 받아온 값으로 업데이트
+    todo.task = task
+    # todo 의 completed 속성을 폼에서 받아온 completed 값으로 업데이트
+    todo.completed = completed
+    
+    db.commit()
+    
+    # 홈으로 리디렉션
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
+
+
+@app.get("/delete/{todo_id}")
+async def delete(todo_id:int, db:Session = Depends(get_db)):
+    
+    todo = db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+    
+    db.delete(todo)
+    
+    db.commit()
+    return RedirectResponse(url=app.url_path_for("home"), status_code=status.HTTP_303_SEE_OTHER)
